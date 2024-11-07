29638c885e27c2e6ca298832f8e6eefa61dc8b9e
saiyadaz <161524327+saiyadaz@users.noreply.github.com>
2024-11-07 10:45:53 -0500

    edit

--- images/01-http-content/run.sh
+++ images/01-http-content/run.sh
@@ -1,5 +1,6 @@
 mkdir -p /usr/share/nginx/html
-while true ; do
-  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
-  sleep 1
-done
+#while true ; do
+#  echo "<h1>Hello - $(date)</h1>" >/usr/share/nginx/html/index.html
+ # sleep 1
+#done
+echo Hello world >/usr/share/nginx/html/index.html
